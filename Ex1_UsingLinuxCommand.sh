6f3bed48a50fa5761d87e11e1a34ce9e6670bd64
Jeyaraman R <rjayavrp2@gmail.com>
2016-08-20 23:37:54 +0100

    Using Linux Commands in script

---
+++ Ex1_UsingLinuxCommand.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "The files/dirs in current directory is..\n"
+ls -la
+
+echo "-----------------------------------------"
+echo "System Uptime :"
+uptime
+echo "-----------------------------------------"
+echo "kernel version :"
+uname -r
+echo "-----------------------------------------"
+echo "All other info :"
+uname -a
+
